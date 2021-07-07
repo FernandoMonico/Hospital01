e02f730e4b1f6ee82bf3dbe6b5b4a99367ede849
fer_lopez <fjlm0002@gmail.com>
2021-07-06 21:20:45 -0600

    Crea Index de Persona Crear vista Index de persona utilizando join en consulta

---
+++ Hospital01/Controllers/PersonaController.cs
@@ -0,0 +1,31 @@
+﻿using Hospital01.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Hospital01.Dto;
+
+namespace Hospital01.Controllers
+{
+    public class PersonaController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<PersonaDto> personaDtoList = new List<PersonaDto>();
+            using (BDHospitalContext db = new BDHospitalContext()) {
+                personaDtoList = (from persona in db.Persona
+                                  join sexo in db.Sexo on persona.Iidsexo equals sexo.Iidsexo
+                                  where persona.Bhabilitado == 1
+                                  select new PersonaDto
+                                  {
+                                      Id = persona.Iidpersona,
+                                      Email = persona.Email,
+                                      NombreCompleto = string.Format("{0} {1} {2}", persona.Nombre, persona.Appaterno, persona.Apmaterno),
+                                      NombreSexo = sexo.Nombre
+                                  }).ToList();
+            }
+                return View(personaDtoList);
+        }
+    }
+}
---
+++ Hospital01/Dto/PersonaDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hospital01.Dto
+{
+    public class PersonaDto
+    {
+        public int Id { get; set; }
+        [Display(Name = "Nombre")]
+        public string NombreCompleto { get; set; }
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+        [Display(Name = "Sexo")]
+        public string NombreSexo { get; set; }
+    }
+}
