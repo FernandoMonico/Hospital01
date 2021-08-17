878ea19038d804fc5e893b77f37a8613a51b3e90
fer_lopez <fjlm0002@gmail.com>
2021-08-17 11:34:59 -0600

    Agregar vista Index de Entidad Medicamento

---
+++ Hospital01/Dto/MedicamentoDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hospital01.Dto
+{
+    public class MedicamentoDto
+    {
+        [Display(Name="Identificador")]
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public decimal? Precio { get; set; }
+        public int? Stock { get; set; }
+        [Display(Name="Forma Farmaceutica")]
+        public string FormaFarmaceutica { get; set; }
+    }
+}
--- Hospital01/Dto/PaginaDto.cs
+++ Hospital01/Dto/PaginaDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,9 +8,12 @@ namespace Hospital01.Dto
 {
     public class PaginaDto
     {
+        [DisplayName("Identificador")]
         public int Id { get; set; }
         public string Mensaje { get; set; }
+        [DisplayName("Acción")]
         public string Action { get; set; }
+        [DisplayName("Controlador")]
         public string Controller { get; set; }
     }
 }
