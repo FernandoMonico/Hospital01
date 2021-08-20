fdf539eefa803ec0c9a0cb15b136af6674f7f794
fer_lopez <fjlm0002@gmail.com>
2021-08-19 22:02:38 -0600

    Agrega filtro por nombre a vista Index de Sede

--- Hospital01/Controllers/SedeController.cs
+++ Hospital01/Controllers/SedeController.cs
@@ -6,16 +6,31 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using LinqKit;
 
 namespace Hospital01.Controllers
 {
     public class SedeController : Controller
     {
-        IMapper _mapper;
-        public SedeController(IMapper mapper) {
+        private readonly IMapper _mapper;
+        private readonly BDHospitalContext _context;
+        public SedeController(IMapper mapper, BDHospitalContext context) {
             _mapper = mapper;
+            _context = context;
         }
-        public IActionResult Index()
+
+        public IActionResult Index(SedeDto sedeDto) {
+            var result = _context.Sede.AsNoTracking().AsExpandable();
+            if (sedeDto.Nombre != null && sedeDto.Nombre != string.Empty) {
+                result = result.Where(x => x.Nombre.Contains(sedeDto.Nombre));
+                ViewBag.NombreSede = sedeDto.Nombre;
+            }
+            var sedeDtoList = result.Select(x => _mapper.Map<SedeDto>(x)).ToList();
+            return View(sedeDtoList);
+        }
+
+        public IActionResult Index_2()
         {
             List<SedeDto> sedeDtoList = new List<SedeDto>();
             using (BDHospitalContext db = new BDHospitalContext())
