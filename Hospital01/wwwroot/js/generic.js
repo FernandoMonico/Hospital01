82a2b03bd629c6dedc72226acbc9ad17864c7ce9
fer_lopez <fjlm0002@gmail.com>
2021-09-01 22:34:37 -0600

    Agrega implementacion de SweetAlert2 Incorporar archivo js correspondiente a SweetAlert2 Crear archivo js generic con funcion encargade de mostrar alertas de confirmación para guardado de registros Agregar alerta de confirmacion en vista Create de Especialidad

---
+++ Hospital01/wwwroot/js/generic.js
@@ -0,0 +1,12 @@
+﻿function ConfirmSave(title = "¿Seguro/a que desea guardar este registro?", text = "Este proceso es irreversible") {
+    return Swal.fire({
+        title: title,
+        text: text,
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Si, guardar',
+        cancelButtonText: 'Cancelar'
+    });
+}
